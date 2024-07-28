fe230891e922bd0beef1483fbb3e03cc0d295cd1
Kk <iankk0806@gmail.com>
2024-07-28 16:16:49 +0800

    7_28

--- candy18.js
+++ candy18.js
@@ -2,8 +2,12 @@
 // 程式語言：JavaScript
 // 題目：實作一個可以印出隨機整數的函數
 
-function randomNumber(num) {
-  Math.random(num);
+function randomNumber(num1, num2) {
+  if (arguments.length == 1) {
+    return Math.floor(Math.random() * num1);
+  } else if (arguments.length == 2) {
+    return Math.floor(Math.random() * (num2 - num1)) + num1;
+  }
 }
 
 console.log(randomNumber(50)); // 隨機印出 0 ~ 49 之間的任何一個數字
--- ss.js
+++ ss.js
@@ -14,24 +14,41 @@
 // console.log(squareDigits(2112)); // 印出 4114
 // console.log(squareDigits(387)); // 印出 96449
 
-const numberReducer = (num) => {
-  // 將數字轉換為字串，然後轉換為數字陣列
-  const numToStr = num.toString();
-  const numArr = Array.from(numToStr, Number);
+// const numberReducer = (num) => {
+//   // 將數字轉換為字串，然後轉換為數字陣列
+//   const numToStr = num.toString();
+//   const numArr = Array.from(numToStr, Number);
 
-  // 計算數字陣列的總和
-  const sum = numArr.reduce((acc, cur) => acc + cur, 0); // 初始值為0
+//   // 計算數字陣列的總和
+//   const sum = numArr.reduce((acc, cur) => acc + cur, 0); // 初始值為0
 
-  // 如果總和小於10，直接返回
-  if (sum < 10) {
-    return sum;
+//   // 如果總和小於10，直接返回
+//   if (sum < 10) {
+//     return sum;
+//   }
+
+//   // 否則遞迴調用 numberReducer，將新的總和作為參數
+//   return numberReducer(sum);
+// };
+
+// console.log(numberReducer(9527)); // 印出 5
+// console.log(numberReducer(1450)); // 印出 1
+// console.log(numberReducer(5566108)); // 印出 4
+// console.log(numberReducer(1234567890)); // 印出 9
+
+function divisors(integer) {
+  const arr = [];
+  let num = 2;
+  while (integer > num) {
+    if (integer % num == 0) {
+      arr.push(num);
+    } else if (num == integer) {
+      break;
+    } else {
+      num++;
+    }
   }
+  return arr;
+}
 
-  // 否則遞迴調用 numberReducer，將新的總和作為參數
-  return numberReducer(sum);
-};
-
-console.log(numberReducer(9527)); // 印出 5
-console.log(numberReducer(1450)); // 印出 1
-console.log(numberReducer(5566108)); // 印出 4
-console.log(numberReducer(1234567890)); // 印出 9
+console.log(divisors(15));
