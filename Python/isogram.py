8ad61cdbeb34ffdafc86a6455a11aac4547a071f
Kk <iankk0806@gmail.com>
2024-10-30 20:02:10 +0800

    10.30_python_practice

---
+++ Python/isogram.py
@@ -0,0 +1,13 @@
+#檢查字串是否為無重複字母的單字。
+#is_isogram("Ddeqrg") -> False
+#is_isogram("erisoq") -> True
+
+
+def is_isogram(string):
+    lower_string=string.lower()
+    seen=[]
+    for char in lower_string:
+        if char in seen:
+            return False
+        seen.append(char)
+    return True
