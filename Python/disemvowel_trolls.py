d44111b39346ee61db5934e4f6aca9dc6a67accd
Kk <iankk0806@gmail.com>
2024-11-19 23:27:39 +0800

    11_19_Python_practice

--- Python/disemvowel_trolls.py
+++ Python/disemvowel_trolls.py
@@ -2,10 +2,18 @@
 
 #For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
 
-def disemvowel(string_):
-  vowels=["a","e","i","o","u","A","E","I","O","U"] #排除母音
-  check_list=[ x for x in string_ if x not in vowels] #string 原本為字串，但進入推倒式時，會被分割成
-  return "".join(check_list)
+# def disemvowel(string_):
+#   vowels=["a","e","i","o","u","A","E","I","O","U"] #排除母音
+#   check_list=[ x for x in string_ if x not in vowels] #string 原本為字串，但進入推倒式時，會被分割成
+#   return "".join(check_list)
     
 
-disemvowel("What the hell")
+# disemvowel("What the hell")
+
+
+
+def shortcut( s ):
+    # ...
+    vowel=['a','e','i','o','u']
+    check_list=[x for x in s if x not in vowel]
+    return ''.join(check_list)
